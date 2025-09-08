7de94bec5b5131d57f21c8587cd7794182a3db98
Activity_Lifecycle <natan.aw95@gmail.com>
2025-09-08 07:33:40 +0700

    Commit No. 4 - Standard activity and single top activity

--- app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
+++ app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
@@ -3,13 +3,13 @@ package com.example.lab_week_02_c
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class StandardActivity : AppCompatActivity() {
     companion object {
         private const val DEBUG = "DEBUG"
     }
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_standard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -25,20 +25,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         Log.d(DEBUG, "onCreate")
-
-        val buttonClickListener = View.OnClickListener {
-            view ->
-            when(view.id) {
-                R.id.button_standard -> startActivity(
-                    Intent(this,
-                        StandardActivity::class.java)
+        findViewById<Button>(R.id.button_standard).setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    StandardActivity::class.java
                 )
-
-                R.id.button_single_top -> startActivity(
-                    Intent(this,
-                        SingleTopActivity::class.java)
-                )
-            }
+            )
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d(DEBUG, "onNewIntent")
     }
 }
